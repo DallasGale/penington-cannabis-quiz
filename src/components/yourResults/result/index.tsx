a11fdd7a822b5d45a3f8ec8ff2bfed5297b52396
Dallas Gale <dallasgale.digital@gmail.com>
2024-11-28 22:47:40 +1100

    wip

--- src/components/yourResults/result/index.tsx
+++ src/components/yourResults/result/index.tsx
@@ -17,42 +17,42 @@ const Result = ({ result, dataSource, explaination }: ResultProps) => {
     <div className={styles.container}>
       {isMobile && (
         <Accordion
-          motionProps={{
-            variants: {
-              enter: {
-                y: 0,
-                opacity: 1,
-                height: "auto",
-                transition: {
-                  height: {
-                    type: "spring",
-                    stiffness: 500,
-                    damping: 30,
-                    duration: 1,
-                  },
-                  opacity: {
-                    easings: "ease",
-                    duration: 1,
-                  },
-                },
-              },
-              exit: {
-                y: -10,
-                opacity: 0,
-                height: 0,
-                transition: {
-                  height: {
-                    easings: "ease",
-                    duration: 0.25,
-                  },
-                  opacity: {
-                    easings: "ease",
-                    duration: 0.3,
-                  },
-                },
-              },
-            },
-          }}
+        // motionProps={{
+        //   variants: {
+        //     enter: {
+        //       y: 0,
+        //       opacity: 1,
+        //       height: "auto",
+        //       transition: {
+        //         height: {
+        //           type: "spring",
+        //           stiffness: 500,
+        //           damping: 30,
+        //           duration: 1,
+        //         },
+        //         opacity: {
+        //           easings: "ease",
+        //           duration: 1,
+        //         },
+        //       },
+        //     },
+        //     exit: {
+        //       y: -10,
+        //       opacity: 0,
+        //       height: 0,
+        //       transition: {
+        //         height: {
+        //           easings: "ease",
+        //           duration: 0.25,
+        //         },
+        //         opacity: {
+        //           easings: "ease",
+        //           duration: 0.3,
+        //         },
+        //       },
+        //     },
+        //   },
+        // }}
         >
           <AccordionItem
             indicator={<div />}
@@ -87,42 +87,42 @@ const Result = ({ result, dataSource, explaination }: ResultProps) => {
             </p>
           </div>
           <Accordion
-            motionProps={{
-              variants: {
-                enter: {
-                  y: 0,
-                  opacity: 1,
-                  height: "auto",
-                  transition: {
-                    height: {
-                      type: "spring",
-                      stiffness: 500,
-                      damping: 30,
-                      duration: 1,
-                    },
-                    opacity: {
-                      easings: "ease",
-                      duration: 1,
-                    },
-                  },
-                },
-                exit: {
-                  y: -10,
-                  opacity: 0,
-                  height: 0,
-                  transition: {
-                    height: {
-                      easings: "ease",
-                      duration: 0.25,
-                    },
-                    opacity: {
-                      easings: "ease",
-                      duration: 0.3,
-                    },
-                  },
-                },
-              },
-            }}
+          // motionProps={{
+          //   variants: {
+          //     enter: {
+          //       y: 0,
+          //       opacity: 1,
+          //       height: "auto",
+          //       transition: {
+          //         height: {
+          //           type: "spring",
+          //           stiffness: 500,
+          //           damping: 30,
+          //           duration: 1,
+          //         },
+          //         opacity: {
+          //           easings: "ease",
+          //           duration: 1,
+          //         },
+          //       },
+          //     },
+          //     exit: {
+          //       y: -10,
+          //       opacity: 0,
+          //       height: 0,
+          //       transition: {
+          //         height: {
+          //           easings: "ease",
+          //           duration: 0.25,
+          //         },
+          //         opacity: {
+          //           easings: "ease",
+          //           duration: 0.3,
+          //         },
+          //       },
+          //     },
+          //   },
+          // }}
           >
             <AccordionItem
               key="1"
