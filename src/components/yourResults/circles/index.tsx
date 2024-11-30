5219ec3525c2e290f78ad8809bde5ad209bdac10
Dallas Gale <dallasgale.digital@gmail.com>
2024-11-30 13:59:36 +1100

    qa

--- src/components/yourResults/circles/index.tsx
+++ src/components/yourResults/circles/index.tsx
@@ -1,17 +1,51 @@
 import styles from "./styles.module.scss";
+import { motion } from "motion/react";
 interface CirclesProps {
   result: number;
 }
 const Circles = ({ result }: CirclesProps) => {
+  const leftCircleVariants = {
+    hidden: { x: "50%" },
+    initial: { x: "50%" },
+    visible: {
+      x: `${result / 2}%`,
+      transition: {
+        type: "spring",
+        bounce: 0.5,
+        duration: 3,
+        ease: [0, 0.71, 0.2, 1.01],
+      },
+    },
+  };
+
+  const rightCircleVariants = {
+    hidden: { x: "-50%" },
+    initial: { x: "-50%" },
+    visible: {
+      x: `-${result / 2}%`,
+      transition: {
+        type: "spring",
+        bounce: 0.5,
+        duration: 3,
+        ease: [0, 0.71, 0.2, 1.01],
+      },
+    },
+  };
   return (
     <div className={styles.container}>
-      <div
+      <motion.div
+        variants={leftCircleVariants}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
         className={styles.circle}
-        style={{ transform: `translateX(${result / 2}%)` }}
-      ></div>
-      <div
+      />
+      <motion.div
+        variants={rightCircleVariants}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
         className={styles.circle}
-        style={{ transform: `translateX(-${result / 2}%)` }}
       />
     </div>
   );
