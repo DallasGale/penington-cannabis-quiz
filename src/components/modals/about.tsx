2f367f4cb1902d86ae8441fef9ab2e5c482a34b1
Dallas Gale <dallasgale.digital@gmail.com>
2024-11-30 14:12:58 +1100

    qa

--- src/components/modals/about.tsx
+++ src/components/modals/about.tsx
@@ -1,8 +1,5 @@
 import { Modal } from "react-responsive-modal";
 import styles from "./styles.module.scss";
-import ModalCta from "../buttons/modalCta";
-import { useRef, useState } from "react";
-import { useIsTablet } from "../../hooks/useIsTablet";
 import SecondaryCta from "../buttons/secondaryCta";
 
 interface AboutModalProps {
@@ -10,39 +7,30 @@ interface AboutModalProps {
   handleOnClick: () => void;
 }
 const AboutModal = ({ open, handleOnClick }: AboutModalProps) => {
-  const myRef = useRef(null);
-
-  const isTablet = useIsTablet();
-
   return (
-    <>
-      {isTablet && <div className="about-modal-container" ref={myRef} />}
-
-      <Modal
-        classNames={{ modal: styles.modal }}
-        center={!isTablet}
-        open={open}
-        onClose={handleOnClick}
-        closeOnEsc={true}
-        closeOnOverlayClick={true}
-        container={myRef.current}
-      >
-        <div className={styles.modalContent}>
-          <h2 className="display3 color-primary">About Us</h2>
-          <div style={{ textAlign: "left" }}>
-            <p className="body color-primary">
-              Penington Institute is an independent drug policy non-profit. We
-              are seeking engagement from the community about views on cannabis
-              regulation as we develop a regulatory framework for safe adult-use
-              cannabis access in Victoria.
-            </p>
-          </div>
-          <div className={styles.ctaGroup}>
-            <SecondaryCta label="Got it" onClick={handleOnClick} />
-          </div>
+    <Modal
+      classNames={{ modal: `${styles.modal} ${styles.aboutModal}` }}
+      center
+      open={open}
+      onClose={handleOnClick}
+      closeOnEsc={true}
+      closeOnOverlayClick={true}
+    >
+      <div className={styles.modalContent}>
+        <h2 className="display3 color-primary">About Us</h2>
+        <div style={{ textAlign: "left" }}>
+          <p className="body4 color-primary">
+            Penington Institute is an independent drug policy non-profit. We are
+            seeking engagement from the community about views on cannabis
+            regulation as we develop a regulatory framework for safe adult-use
+            cannabis access in Victoria.
+          </p>
         </div>
-      </Modal>
-    </>
+        <div className={styles.ctaGroup}>
+          <SecondaryCta label="Got it" onClick={handleOnClick} />
+        </div>
+      </div>
+    </Modal>
   );
 };
 
