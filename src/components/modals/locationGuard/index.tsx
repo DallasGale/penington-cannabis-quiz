a27f023de461778403b8a5709480ac0a95ab9a60
Dallas Gale <dallasgale.digital@gmail.com>
2024-12-05 15:31:18 +1100

    added '!'

--- src/components/modals/locationGuard/index.tsx
+++ src/components/modals/locationGuard/index.tsx
@@ -1,7 +1,5 @@
 import { Modal } from "react-responsive-modal";
 import styles from "../styles.module.scss";
-import { useEffect, useState } from "react";
-import SecondaryCta from "../../buttons/secondaryCta";
 import React from "react";
 const LocationGuardModal = () => {
   return (
@@ -13,7 +11,7 @@ const LocationGuardModal = () => {
       closeOnOverlayClick={false}
     >
       <div className={styles.modalContent}>
-        <h2 className="display3 color-primary">Sorry</h2>
+        <h2 className="display3 color-primary">Sorry!</h2>
         <p className="body4 color-primary">
           This website is currently available to Australian residents only. We
           apologise for the inconvenience.
