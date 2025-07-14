dcaa1fd5341f77834e2b51ea74a17ef1aedbe8d3
Dallas Gale <dallasgale.digital@gmail.com>
2025-07-15 09:56:03 +1000

    reverted config change

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,11 +9,11 @@ export default defineConfig({
   integrations: [react()],
   base: process.env.PUBLIC_BASE_URL,
   outDir: process.env.BUILD_OUTPUT_DIR,
-  // output: "server", // beacuse there are serverless
+  output: "server", // beacuse there are serverless
   // functions involced in this project you will most
   // likely need to output with this "server" option instead.
   // If so, you will need to make sure your server is setup for cloud functions.
-  output: "static",
+  // output: "static",
   adapter: vercel(),
   vite: {
     envPrefix: "NEXT_PUBLIC_",
